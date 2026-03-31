5c57f54cf69bce4bdf4e3e1a31397154874c98e7
Qutub Ahmed <qutubahmedkhan@outlook.com>
2026-04-01 01:32:30 +0500

    once again commit

--- api/index.js
+++ api/index.js
@@ -47,4 +47,6 @@ app.post("/analyze", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// app.listen(3000, () => console.log("Server running on port 3000"));  
+
+export default app;
